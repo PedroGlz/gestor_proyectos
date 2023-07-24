b343a634bc735f8837c3c83df8ceda46ff11bca5
Pedro Gonzalez <gpedro.17.3f@gmail.com>
2023-07-24 06:12:54 -0600

    candados y estilos alert listos

--- public/js/paginas/global.js
+++ public/js/paginas/global.js
@@ -1,14 +1,14 @@
 // variable cofiguracion sweetalert2
 const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',    
-    iconColor: 'white',
-    customClass: {
-      popup: 'colored-toast'
-    },
-    showConfirmButton: false,
-    timer: 2500,
-    timerProgressBar: false,
+  toast: true,
+  position: 'top-right',    
+  iconColor: 'white',
+  customClass: {
+    popup: 'colored-toast'
+  },
+  showConfirmButton: false,
+  timer: 2500,
+  timerProgressBar: false,
 })
 
 /* Alerta que muestra mensaje de cargando */
